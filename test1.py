28eda9a6a52e2bd95c5bc1d7b5802e0c8824de4b
Kit <yayekit@gmail.com>
2024-10-26 14:55:47 -0400

    added test files and fixed the env

---
+++ test1.py
@@ -0,0 +1,19 @@
+# Імпортуємо всі необхідні функції
+from ab_test_analysis import *
+
+# Запускаємо аналіз
+results = run_analysis('task1.csv')
+
+# Дивимось основні метрики
+print("\nОсновні метрики:")
+print(results['metrics'])
+
+# Дивимось результати статистичних тестів
+print("\nРезультати статистичних тестів:")
+print("Trial Test p-value:", results['statistical_tests']['trial_test'][1])
+print("Paid Test p-value:", results['statistical_tests']['paid_test'][1])
+print("Revenue Test p-value:", results['statistical_tests']['revenue_test'].pvalue)
+
+# Дивимось метрики по країнах
+print("\nМетрики по країнах:")
+print(results['country_metrics'])   
